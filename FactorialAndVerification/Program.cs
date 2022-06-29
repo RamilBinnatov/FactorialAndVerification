7699e8ffa7db3f9f404a5e793cc76b6c79ab2979
RamilBinnatov <ramilrb@code.edu.az>
2022-06-29 04:49:49 +0400

    FactorialAndVerification

---
+++ FactorialAndVerification/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialAndVerification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter a number: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int fact = 1;
+            //Factorial(n, fact);
+
+            //Console.WriteLine("Enter mail address : ");
+            //string mail = Convert.ToString(Console.ReadLine());
+
+            //Console.WriteLine("Enter password: ");
+            //int passWord = Convert.ToInt32(Console.ReadLine());
+            //Verification(mail, passWord);
+        }
+
+        static void Factorial(int n , int fact)
+        {
+            for (int i = 1; i <= n; i++)
+            {  
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial:" + fact);
+        }
+
+        static void Verification(string mail, int passWord)
+        {
+
+
+            if (mail == "cavid@code.edu.az" && passWord == 12345)
+            {
+                Console.WriteLine("Wellcome");
+            }
+            else
+            {
+                Console.WriteLine("mail or password is uncorrect");
+            }
+                    
+
+
+            
+        }
+
+        
+    }
+
+}
